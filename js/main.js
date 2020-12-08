67b5b9b430bd4b38dc68372921c107c3b2387f9e
starahmad6161 <starahmad616161@gmail.com>
2020-12-08 02:57:30 +0200

    sas

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    'use strict';
+
+    /*
+    $(".navbar-light .navbar-nav .nav-link").on('mouseenter', function() {
+
+        $(this).find(".dropdown-menu").addClass("show");
+
+    });
+    */
+});
